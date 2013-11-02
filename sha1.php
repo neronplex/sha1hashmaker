9156e9a2c0cf9f57ef235e3cf129e96550f23854
暖簾 <github@sv.perfectdark.jp>
2013-11-03 02:16:27 +0900

    refactoring:add comment

--- sha1.php
+++ sha1.php
@@ -1,21 +1,23 @@
 <?php
+// セッション開始
 session_start();
 
 // CSRF対策トークン生成関数
 function settoken() {
-	$token = sha1(uniqid(mt_rand(),true));
+	$token = sha1(uniqid(mt_rand(),true)); # sha1
 	$_SESSION[token] = $token;
 }
 
 // トークンチェック関数
 function checktoken() {
-	if(empty($_SESSION[token]) || ($_SESSION[token] != $_POST[token]))
+	if(empty($_SESSION[token]) || ($_SESSION[token] != $_POST[token])) # トークンが空かもしくはPOSTされたトークンと値が違う
 	{
 	print('不正な投稿が行われました。');
 	exit;
 	}
 }
 
+// POSTされたときの処理
 if($_SERVER[REQUEST_METHOD] != 'POST')
 	{
 	// CSRF対策フロー
@@ -34,8 +36,8 @@ if($_SERVER[REQUEST_METHOD] != 'POST')
 	$err[string] = 'ハッシュ化したい文字列が入力されていません。';
 	}
 
+	//ハッシュ化
 	if(empty($err)){
-		//ハッシュ化
 		$hash = sha1($string);
 		$hashed = "ハッシュ化した文字列 : $hash";
 	}
@@ -46,17 +48,28 @@ if($_SERVER[REQUEST_METHOD] != 'POST')
 <!DOCTYPE html>
 <html>
 <head>
+<!- 文字化け対策の文字コード ->
 <meta charset="UTF-8">
 <title>sha1 hash maker</title>
 </head>
 
 <body>
+<!- タイトル ->
 <h1>sha1 hash maker</h1>
+
+<!- 入力フォーム ->
 <form action="" method="POST">
-<input type="hidden" name="token" value="<?php echo htmlspecialchars($_SESSION[token],ENT_QUOTES,""); ?>">
-<p>ハッシュ化したい文字列 <input type="text" name="string" value="<?php echo $string; ?>"> <?php echo $err[string]; ?></p>
-<p><?php echo htmlspecialchars($hashed,ENT_QUOTES,""); ?></p>
-<input type="submit" value="ハッシュ化">
+	<!- CSRF対策トークン ->
+	<input type="hidden" name="token" value="<?php echo htmlspecialchars($_SESSION[token],ENT_QUOTES,""); ?>">
+
+	<!- 文字列入力部分 ->
+	<p>ハッシュ化したい文字列 <input type="text" name="string" value="<?php echo $string; ?>"> <?php echo $err[string]; ?></p>
+
+	<!- ハッシュ値出力部分 ->
+	<p><?php echo htmlspecialchars($hashed,ENT_QUOTES,""); ?></p>
+
+	<- submitボタン ->
+	<input type="submit" value="ハッシュ化">
 </form>
 </body>
 </html>
